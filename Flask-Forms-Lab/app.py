92aed7318085d79befb95e4cf69a201467df5cca
AdeemSh24-meet <139681300+AdeemSh24-meet@users.noreply.github.com>
2023-07-23 18:17:32 +0300

    still working

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -6,16 +6,37 @@ app = Flask(  # Create a flask app
 	template_folder='templates',  # Name of html file folder
 	static_folder='static'  # Name of directory for static files
 )
-
-
-username = "siwarha"
+#app.config['password'] = 123
 password = "123"
+
+
+app.config['password'] = 'password'
+
 facebook_friends=["Loai","Kenda","Avigail", "George", "Fouad", "Gi"]
 
-
-@app.route('/', methods==[post])  # '/' for the default page
+@app.route('/')  # '/' for the default page
 def login():
   return render_template('login.html')
+
+
+# @app.route('/creat')  # '/' for the default page
+# def home():
+#   return render_template('home.html')
+
+# @app.route('/create, methods=['GET', 'POST'])
+# def home():
+#     if request.method == 'post':
+#         return render_template('home.html')
+
+@app.route('/create',methods=['GET','POST'])
+def create():
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		username = request.form['username']
+		password = request.form['password']
+		return render_template('home.html')
+
   
 
 
